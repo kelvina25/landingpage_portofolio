3910b6cb65bec475d3954b56c1130e86f2a0081c
kelvinantonio <kelvin.antonio@student.umn.ac.id>
2023-08-05 23:36:02 +0700

    firstcommit

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export const Social = () => {
+  return (
+    <div className="home__social">
+        <a href="https://www.instagram.com/kelvin.antonio56/" className="home__social-icon" target="_blank">
+        <i class="uil uil-instagram"></i>
+        </a>
+
+        <a href="https://www.linkedin.com/in/kelvin-antonio-862479261/" className="home__social-icon" target="_blank">
+        <i class="uil uil-linkedin"></i>
+        </a>
+
+        <a href="https://github.com/kelvina25" className="home__social-icon" target="_blank">
+        <i class="uil uil-github"></i>
+        </a>
+
+    </div>
+  )
+}
+
+export default Social
